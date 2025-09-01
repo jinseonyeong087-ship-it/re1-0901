83a95c56d865cd17757f588884172f1a6e00d298
jinseonyeong087-ship-it <jinseonyeong087@gmail.com>
2025-09-01 16:01:15 +0900

    0901

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <p>처음 실행 성공 🎉</p>
+    </div>
+  );
+}
+
+export default App;
